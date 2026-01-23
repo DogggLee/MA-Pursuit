df25e3734328215a8d386b40fcbd7d19d60ae928
DogggLee <627921700@qq.com>
2026-01-23 16:55:51 +0800

    Add new exp dir func

--- train_base.py
+++ train_base.py
@@ -16,13 +16,23 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('config_path', type=str, default="config/base.yaml", help='Exp config file path')
-parser.add_argument('--dump_root', type=str, help='Checkpoint path')
+parser.add_argument('--dump_root', type=str, default="checkpoints", help='Checkpoint path')
 parser.add_argument('--checkpoint', type=str, help='Checkpoint path')
 
 def main(args):
     config = load_config(args.config_path)
 
+    exp_dirname = generate_exp_dirname(config)
+
+    exp_root = Path(args.dump_root) / exp_dirname
+    exp_root.makedirs_p()
+
+    set_global_seeds(config.Base.seed)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Using device: {device}")
+
     
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
